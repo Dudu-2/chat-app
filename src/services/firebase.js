33aa03b3b1a387e0b0e4f1581168483c86566d8d
Dudu-2 <carlosbarbosa0556@gmail.com>
2022-12-07 16:19:02 -0300

    limpo

---
+++ src/services/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCB6Uyrfal7ZppC7pOoz5nMLjZ34nBgdn4',
+  authDomain: 'react-chat-app-fef77.firebaseapp.com',
+  projectId: 'react-chat-app-fef77',
+  storageBucket: 'react-chat-app-fef77.appspot.com',
+  messagingSenderId: '1010689354382',
+  appId: '1:1010689354382:web:5bee65bb462184177cf7df',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GithubAuthProvider();
+
+export { db, auth, provider };
